aa06b2943483710a2bded046acc971e27eb52abc
Shadeed Bilal <lildee2020@gmail.com>
2026-03-10 21:24:07 -0400

    Add project files.

---
+++ MovieList/Migrations/20260202002733_AddGenreField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieList.Migrations
+{
+    public partial class AddGenreField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 4,
+                column: "Year",
+                value: 1942);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 4,
+                column: "Year",
+                value: 1943);
+        }
+    }
+}
---
+++ MovieList/Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieList.Models
+{
+    public class Genre
+   {
+    //   public string GenreId { get; set; } = string.Empty;
+       public string Name { get; set; } = string.Empty;
+ //   }
+
+   // {
+public string GenreId { get; set; }
+  //  public string Name { get; set; }
+}
+}
